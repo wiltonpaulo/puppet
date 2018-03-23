b6d7c3c7b378cf0616be4f34a443cef4f580764d
root <root@puppet.tecdump.com>
2018-03-22 21:14:47 -0300

    primeiro puppet

---
+++ manifests/site.pp
@@ -0,0 +1,16 @@
+node 'hub', cloud1, {    # applies to ns1 and ns2 nodes
+  file {'/tmp/dns':    # resource type file and filename
+    ensure => present, # make sure it exists
+    mode => '0644',
+    content => "Only DNS servers get this file. CLOUD\n",
+  }
+}
+node 'admin', 'puppet' {    # applies to ns1 and ns2 nodes
+  file {'/tmp/dns':    # resource type file and filename
+    ensure => present, # make sure it exists
+    mode => '0644',
+    content => "puppetserver. 22032018\n",
+  }
+}
+
+node default {}       # applies to nodes that aren't explicitly defined
