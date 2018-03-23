90c873e05ed10af6e830cd28e3e8e41b89e30809
Wilton Paulo da Silva <wilton.paulo@oi.net.br>
2018-03-23 09:23:05 -0300

    teste desktop

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,7 +5,7 @@ node 'hub', 'cloud1', 'cloud2', 'cloud3' {    # applies to ns1 and ns2 nodes
     content => "so na nuvem 22h07. CLOUD\n",
   }
 }
-node 'admin', 'puppet' {    # applies to ns1 and ns2 nodes
+node 'admin', 'puppet', 'sites' {    # applies to ns1 and ns2 nodes
   file {'/tmp/dns':    # resource type file and filename
     ensure => present, # make sure it exists
     mode => '0644',
