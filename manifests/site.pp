8108fcd2778808c949fc883b8e17d68e9959fd53
wiltonpaulo <wiltonpaulo@gmail.com>
2018-03-22 22:07:18 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,14 +2,14 @@ node 'hub', 'cloud1', 'cloud2', 'cloud3' {    # applies to ns1 and ns2 nodes
   file {'/tmp/dns':    # resource type file and filename
     ensure => present, # make sure it exists
     mode => '0644',
-    content => "Only DNS servers get this file. CLOUD\n",
+    content => "so na nuvem 22h07. CLOUD\n",
   }
 }
 node 'admin', 'puppet' {    # applies to ns1 and ns2 nodes
   file {'/tmp/dns':    # resource type file and filename
     ensure => present, # make sure it exists
     mode => '0644',
-    content => "puppetserver. 22032018\n",
+    content => "mudou tudo\n",
   }
 }
 
