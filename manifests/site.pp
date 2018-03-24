381b88e502f870fcdd9aebfc4426517613890c7a
root <root@admin.tecdump.com>
2018-03-24 14:13:04 -0300

    admin test

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,6 @@ node default {
       include "role::${::role}"
   } else {
       include profile::base
-      notify { "Node  ${::fqdn} has no role set." }
+      notify { "Node  ${::fqdn} has no role set.": }
   }
-}       # applies to nodes that aren't explicitly defined
+}
