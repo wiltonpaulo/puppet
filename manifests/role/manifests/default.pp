a564c1304382cbafd0b092a588cb5ad844fb094d
Wilton Paulo da Silva <wilton.paulo@oi.net.br>
2018-03-27 16:26:59 -0300

    firewall

--- manifests/role/manifests/default.pp
+++ manifests/role/manifests/default.pp
@@ -1,7 +1,7 @@
 #role/manifests/default.pp
 class role::default {
 	include profile::base
-	firewall { 'ssh http and https access':
+	firewall { '000 ssh http and https access':
 		dport  => [22, 80, 443],
 		proto  => tcp,
 		action => accept,
