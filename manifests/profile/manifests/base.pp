eae39cb3e3b9f1f0b6363f718e35041dd25c5a15
Wilton Paulo da Silva <wilton.paulo@oi.net.br>
2018-03-27 16:23:29 -0300

    config firewall

--- manifests/profile/manifests/base.pp
+++ manifests/profile/manifests/base.pp
@@ -11,7 +11,7 @@ class profile::base {
   case $::osfamily {
     'redhat': {
       include profile::base::redhat
-      include profile::default
+      include role::default
     }
     default: {
       fail("This profile is not supported")
