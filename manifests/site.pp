9f3a44379c43ebdff5faf24c727711fe05a2e1bc
Wilton Paulo da Silva <wilton.paulo@oi.net.br>
2018-03-27 19:42:02 -0300

    upload

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,15 @@
-#$role=hiera('role', true)
-node default {
-  if $::role {
-      include "role::${::role}"
-  } else {
-      include profile::base
-      notify { "Sem ROLE para ${::fqdn}.": }
-  }
+Package {
+  allow_virtual => true,
 }
+node default {
+  hiera_include('classes')
+}
+#$role=hiera('role', true)
+#node default {
+#  if $::role {
+#      include "role::${::role}"
+#  } else {
+#      include profile::base
+#      notify { "Sem ROLE para ${::fqdn}.": }
+#}
+#}
