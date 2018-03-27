9366afb5fa9b4dc1cfcfdef9fecc05bae46e3ee6
Wilton Paulo da Silva <wilton.paulo@oi.net.br>
2018-03-27 17:04:47 -0300

    site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,8 +2,10 @@ $role= hiera('role', false)
 node default {
   if $::role {
       include "role::${::role}"
+      include profile::default
   } else {
       include profile::base
+      include profile::default
       notify { "Sem ROLE para ${::fqdn}.": }
   }
 }
