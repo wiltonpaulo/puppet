7fb8f8a19580911d26918aa29c7737649c2eb85d
root <root@admin.tecdump.com>
2018-04-12 19:57:36 -0300

    update2

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,6 @@ Package {
   allow_virtual => true,
 }
 node default {
-<<<<<<< HEAD
   if $::role {
       include "role::${::role}"
   } else {
@@ -10,16 +9,3 @@ node default {
       notify { "Node  ${::fqdn} has no role set.": }
   }
 }
-=======
-  hiera_include('classes')
-}
-#$role=hiera('role', true)
-#node default {
-#  if $::role {
-#      include "role::${::role}"
-#  } else {
-#      include profile::base
-#      notify { "Sem ROLE para ${::fqdn}.": }
-#}
-#}
->>>>>>> fcef6c9d8fd226991baaddd54bebdb53da01870c
